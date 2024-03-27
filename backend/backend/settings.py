5f0b1e4e5163a79a127f5406e6c0f50f0ab303e1
Alexander Lisin <mr.fox.python@gmail.com>
2024-03-27 13:30:17 +0400

    Push Docker image to Docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,5 @@
-"""This is django."""
-
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foxtaskgolubey.zapto.org', '51.250.28.239', 'localhost', '127.0.0.1']
 
 
 # Application definition
